3bd637b64926c799fced3dbaac33f9064bc536c1
infiniti008 <nikitenok.sl@gmail.com>
2017-05-21 15:27:36 +0300

    neeeew

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 var TelegramBot = require('node-telegram-bot-api');
 var fs = require('fs');
+var path_to_user = './user.json';
+
+var time = [];
+
+function User {
+    this.kjhnkjnkjnkjn
+}
+
+User.prototype.showAge = function() {
+    console.log( this.age)
+}
+
+function readFrom() {
+    this = require(path_to_user).;
+    console.log(this);
+}
+// function writeTo() {
+//     var to_file = JSON.stringify(user);
+//     fs.writeFileSync(path_to_user, to_file);
+//     console.log('Write siccess');
+// }
 
 var command = {
     '/echo': ' - Команда ЭХО.',
-    '/help': ' - Список команд'
+    '/help': ' - Список команд',
+    '/add_time': ' - Добавить время',
+    '/show_time': ' - Показать текущие времена'
 };
 
 var token = '268377689:AAEehpljdqiY6qITewLNPUkbe60Kbszl95w';
@@ -28,10 +51,55 @@ function start_bot() {
         var chatId = msg.chat.id;
         bot.sendMessage(chatId, help);
     });
-
-    bot.on('message', function(msg) {
+    
+    
+    bot.onText(/\/add_time (.+)/, function(msg, match) {
         var chatId = msg.chat.id;
-        console.log(msg);
-        // console.log(add_torr[chatId]);
+        // console.log(match[1].substring(3, 5)*1);
+        if(match[1].length != 5){
+            bot.sendMessage(chatId, 'Введите время в нужном формате - ЧЧ:ММ');
+        }
+        else if(match[1].charAt(2) != ':'){
+            bot.sendMessage(chatId, 'Не забудте разделить часы и минуты символом ":"');
+        }
+        else if((match[1].substring(0, 2) * 1) >= 25){
+            bot.sendMessage(chatId, 'Введите часы в 24-х часовом формате');
+        }
+        else if((match[1].substring(3, 5) * 1) >= 61){
+            bot.sendMessage(chatId, 'Введите минуты в 60-и минутном формате');
+        }
+        else{
+            bot.sendMessage(chatId, 'Мы сохранили ваше время! Можете добавить еще одно время используя ту-же команду /add_time');
+            time.push(match[1]);
+        }
+       
+        
     });
+    
+    bot.onText(/\/add_time/, function(msg, match) {
+        if(msg.text == '/add_time'){
+            var chatId = msg.chat.id;
+            bot.sendMessage(chatId, 'Введите команду в следующем формате: /add_time ЧЧ:ММ');
+        }
+    });
+    
+    bot.onText(/\/show_time/, function(msg, match) {
+            var chatId = msg.chat.id;
+            bot.sendMessage(chatId, time);
+            console.log(time);
+    });
+    
+    
+    
+        bot.on('message', function(msg) {
+            if(msg.entities[0].type != 'bot_command'){
+                var chatId = msg.chat.id;
+                console.log('This is bot command');
+                console.log(msg.text);
+                // console.log(add_torr[chatId]);
+                // readFrom();
+                bot.sendMessage(chatId, 'Uncnow command');
+            }
+        });
+    
 }
